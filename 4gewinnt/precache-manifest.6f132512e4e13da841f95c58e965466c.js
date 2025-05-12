35f9dfe863623cffc8a61357431bd2931a9d1f26
shyMartin <martin@shymartin.dev>
2025-05-12 16:25:00 +0200

    Added 4gewinnt

---
+++ 4gewinnt/precache-manifest.6f132512e4e13da841f95c58e965466c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "180b183989d997b79846ed79128866ba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a37be38f9409fb8db4d9",
+    "url": "/static/css/main.8e7653f7.chunk.css"
+  },
+  {
+    "revision": "8c3d166422cb84e5dd92",
+    "url": "/static/js/2.e18d2df9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e18d2df9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37be38f9409fb8db4d9",
+    "url": "/static/js/main.f882a69a.chunk.js"
+  },
+  {
+    "revision": "1249babb1f9002060aaf",
+    "url": "/static/js/runtime-main.6a2302b0.js"
+  }
+]);
